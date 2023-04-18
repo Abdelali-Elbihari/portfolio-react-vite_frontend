5629ee2ecb5d493056bb16d199a2dd178c5b4144
bihariel <bihariel@gmail.com>
2023-04-19 01:23:20 +0200

    Add NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,47 +1,55 @@
-import React, {useState} from 'react';
-import { images } from '../../constants';
-import { HiMenuAlt4, HiX } from 'react-icons/hi';
-import { motion } from 'framer-motion';
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { HiMenuAlt4, HiX } from 'react-icons/hi'
+import { motion } from 'framer-motion'
+import { images } from '../../constants'
 
-import './NavBar.scss';
+import './NavBar.scss'
+
+const links = ['home', 'about', 'contact', 'work', 'skills']
 
 const NavBar = () => {
   const [toggle, setToggle] = useState(false)
+
   return (
-    <nav className='app__navbar'>
-    <div className='app__navbar-logo'>
-      <img src={images.logo} alt='logo' />
-    </div>
-    <ul className='app__navbar-links'>  
-      { ['home', 'about', 'contact', 'work', 'skills', 'contact'].map(item => (
-        <li className='app__flex p-text' key={`link-${item}`}>
-          <div />
-          <a href={`#${item}`}>{item}</a>
-        </li>
-      ))}
-    </ul>
+    <nav className="app__navbar">
 
-    <div className='app__navbar-menu'>
-      <HiMenuAlt4 onClick={() => setToggle(true)}/>
-      {toggle && (
-        <motion.div
-        whileInView={{ x: [300, 0]}}
-        transition={{ duration: 0.85, ease: 'easeInOut'}}
-        >
-          <HiX onClick={() => setToggle(false)} />
-          { ['home', 'about', 'contact', 'work', 'skills', 'contact'].map(item => (
-            <li key={item}>
-             <a onClick={() => setToggle(false)} href={`#${item}`}>{item}</a>
-           </li>
-          ))}
-        </motion.div>
-      )
-      }
+      {/* Mobile menu */}
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [-200, 0] }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {links.map((link) => (
+                <li key={`menu-${link}`}>
+                  <Link to={`/${link}`} onClick={() => setToggle(false)}>
+                    {link}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
 
-    </div>
+      <ul className="app__navbar-links">
+        {links.map((link) => (
+          <li className="app__flex p-text" key={`link-${link}`}>
+            <div />
+            <Link to={`/${link}`}>{link}</Link>
+          </li>
+        ))}
+      </ul>
 
-  </nav>
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
+      </div>
+    </nav>
   )
-};
+}
 
-export default NavBar;
+export default NavBar
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import { images } from '../../constants'
 import './Header.scss'
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+  return <div>Header</div>
 }
 
 export default Header
