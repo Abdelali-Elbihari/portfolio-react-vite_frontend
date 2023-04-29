7dae9d099c50d46de6d68a2d8dc248546c8d05b9
bihariel <bihariel@gmail.com>
2023-04-29 16:33:29 +0200

    Add compatibility for legacy for browsers that do not support  native ES Modules

--- frontend_react_vite/vite.config.js
+++ frontend_react_vite/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -15,7 +16,12 @@ export default defineConfig(({ mode }) => {
   };
 
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      legacy({
+        targets: ['defaults', 'not IE 11']
+      })
+    ],
     define: envWithProcessPrefix
   };
 });
