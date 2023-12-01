4ff17b0c9c56e427dd205d12fc43f319e95b6e1c
Abdelali Elbihari <bihariel@gmail.com>
2023-12-01 12:18:16 +0100

    Delete backend_sanity directory

--- backend_sanity/schemas/index.ts
+++
@@ -1,8 +0,0 @@
-import abouts from "./abouts";
-import contact from "./contact";
-import experiences from "./experiences";
-import skills from "./skills";
-import workExperience from "./workExperience";
-import projects from "./projects";
-
-export const schemaTypes = [projects, abouts, skills, workExperience, experiences, contact]
